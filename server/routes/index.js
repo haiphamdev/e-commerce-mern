f65b2bd9cd1c962becaae3fb1ee858408725c539
Hai Pham <haipham.ndev@gmail.com>
2025-12-12 10:00:29 +0700

    #3 Handle error

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const useRouter = require("./user");
+const { notFound, errorHandler } = require("../middlewares/errHandler");
 
 const initRoutes = (app) => {
   app.use("/api/user", useRouter);
+
+  app.use(notFound);
+  app.use(errorHandler);
 };
 
 module.exports = initRoutes;
